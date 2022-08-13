252c9a3855070484ba709cc441f81dcd7c8a77d5
Fluffy-Bean <michal-gdula@protonmail.com>
2022-08-13 12:22:29 +0000

    Footer link fix

--- ui/footer.php
+++ ui/footer.php
@@ -11,7 +11,7 @@
     <img class='svg' src='<?php echo $root_dir; ?>assets/icons/github-logo.svg'>
     Github
   </a>
-  <a class='link' href="about.php">
+  <a class='link' href="<?php echo $root_dir; ?>about.php">
     <img class='svg' src='<?php echo $root_dir; ?>assets/icons/scroll.svg'>
     About
   </a>
