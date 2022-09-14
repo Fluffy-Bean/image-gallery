dec15d7b6335874155b65770ea4a0c6dc5c75395
Fluffy-Bean <michal-gdula@protonmail.com>
2022-09-14 16:43:06 +0000

    Give an error when connection dies

--- app/server/conn.php
+++ app/server/conn.php
@@ -1,21 +1,18 @@
 <?php
 /*
-  Connect to database
+	Connect to database
 
-  Dunno what else to put here lol
+	Dunno what else to put here lol
 */
-$conn_ip = "192.168.0.79:3306";
-$conn_username = "uwu";
-$conn_password = "fennec621";
-$conn_database = "gallery";
+try {
+	$conn_ip = "192.168.0.79:3306";
+	$conn_username = "uwu";
+	$conn_password = "fennec621";
+	$conn_database = "gallery";
 
-$conn = mysqli_connect($conn_ip, $conn_username, $conn_password , $conn_database);
-if ($conn->connect_error) {
-  ?>
-    <script>
-      sniffleAdd('Error','Could not make a connection to the server, please try again later','var(--red)','assets/icons/warning.svg');
-    </script>
-  <?php
+	$conn = @mysqli_connect($conn_ip, $conn_username, $conn_password , $conn_database);
+} catch (Exception $e) {
+	header("location: error.php?e=conn");
 }
 
 session_start();
